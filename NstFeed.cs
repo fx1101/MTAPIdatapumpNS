3f8780e27fc5f25c5f4fee6aa37a237a3b6bb607
Chris Satterfield <77019637+fx1101@users.noreply.github.com>
2022-07-22 17:40:36 -0500

    added code files for the initial checkin

---
+++ NstFeed.cs
@@ -0,0 +1,78 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MTAPIdatapumpNS
+{
+    class NstFeed
+    {
+        [DllImport("NSTFeed.dll", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        public static extern int NSTSETUPDATA(string ticker, string description, float barInterval,
+            int numColumns, string category, double firstDateAvailable);
+
+        [DllImport("NSTFeed.dll", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        public static extern int NSTSETLABEL(string ticker, float barInterval, int col, string label);
+
+        [DllImport("NSTFeed.dll", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        public static extern int NSTSETVALUE(string ticker, float barInterval, int col, double val);
+
+        [DllImport("NSTFeed.dll", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        public static extern int NSTCOMMITROW(string ticker, float barInterval);
+
+        [DllImport("NSTFeed.dll", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        public static extern void SetDataFeedhwnd(int hWnd);
+
+        [DllImport("NSTFeed.dll", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        public static extern void UpdatePercent(int Percent);
+
+        [DllImport("NSTFeed.dll", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        public static extern void RequireUserIDPassword();
+
+        [DllImport("NSTFeed.dll", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        public static extern void GetUserIDPassword(ref string UserID, ref string Password);
+
+        // NOTE: The below is unused but a developer could implement if they wanted to
+
+        //[DllImport("kernel32", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        //private static extern int GetTimeZoneInformation(TIME_ZONE_INFORMATION lpTimeZoneInformation);
+
+        ////[STAThread]
+        //public static void Main()
+        //{
+        //    TIME_ZONE_INFORMATION lpTimeZoneInformation;
+        //    lpTimeZoneInformation.DaylightDate.wMonth = 0;
+        //    lpTimeZoneInformation.StandardDate.wMonth = 0;
+        //    int x = GetTimeZoneInformation(lpTimeZoneInformation);
+        //    double SubtractTime = 0.20833333333333334;
+        //    TimeZoneBias = lpTimeZoneInformation.TimeZoneBias;
+        //    TimeZoneBias = (TimeZoneBias / 1440.0) - SubtractTime;
+        //    new DataPump().ShowDialog();
+        //}
+
+        //// Nested Types
+        //[StructLayout(LayoutKind.Sequential)]
+        //private struct SYSTEMTIME
+        //{
+        //    public short wYear;
+        //    public short wMonth;
+        //    public short wDayOfWeek;
+        //    public short wDay;
+        //    public short wHour;
+        //    public short wMinute;
+        //    public short wSecond;
+        //    public short wMilliseconds;
+        //}
+
+        //[StructLayout(LayoutKind.Sequential)]
+        //private struct TIME_ZONE_INFORMATION
+        //{
+        //    public int TimeZoneBias;
+        //    [VBFixedString(64), MarshalAs(UnmanagedType.ByValArray, SizeConst = 64)]
+        //    public char[] StandardName;
+        //    public DataPumpAPI.SYSTEMTIME StandardDate;
+        //    public int StandardTimeZoneBias;
+        //    [VBFixedString(64), MarshalAs(UnmanagedType.ByValArray, SizeConst = 64)]
+        //    public char[] DaylightName;
+        //    public DataPumpAPI.SYSTEMTIME DaylightDate;
+        //    public int DaylightTimeZoneBias;
+        //}
+    }
+}
---
+++ datapumpNS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RGiesecke.DllExport;
+
+namespace MTAPIdatapumpNS
+{
+    public class datapumpNS
+    {
+        [DllExport("UserIDPasswordChanged", CallingConvention = System.Runtime.InteropServices.CallingConvention.StdCall)]
+        public static long UserIDPasswordChanged(string UserID, string Password)
+        {
+            /*char temp[200];
+            strcpy(temp, UserID);
+            strcat(temp, ",");
+            strcat(temp, Password);
+            MessageBox(NULL, temp, "Data Pump DLL Example", 0);*/
+            return -1;
+        }
+
+        [DllExport("RemoveTicker", CallingConvention = System.Runtime.InteropServices.CallingConvention.StdCall)]
+        public static long RemoveTicker(string ticker, float BarInterval)
+        {
+            return 1;
+        }
+
+        [DllExport("RequestTicker", CallingConvention = System.Runtime.InteropServices.CallingConvention.StdCall)]
+        public static long RequestTicker(string ticker, string description, string category, double startdate, float BarInterval, string [] ColumnNames)
+        {
+            return 1;
+        }
+    }
+}
